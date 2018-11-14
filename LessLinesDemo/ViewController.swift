f05c3dc1f2313475de0fcd8f5effdd3ac9393e77
Mustafa Baalbaki <mustafa.baalbaki@gmail.com>
2018-11-14 20:36:39 +0900

    Initial Commit

---
+++ LessLinesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LessLinesDemo
+//
+//  Created by Mustafa Baalbaki on 11/14/18.
+//  Copyright © 2018 Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
